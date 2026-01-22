c6122051c7f4b974e0b8735868b62ec3e112fb76
Terbal <joelmoyo249@gmail.com>
2026-01-22 17:58:59 +0100

    Add login stylé

--- js/auth.js
+++ js/auth.js
@@ -2,6 +2,15 @@ const API_URL = location.hostname === 'localhost'
   ? 'http://localhost:3000'
   : `${location.protocol}//${location.host}`;
 
+document.addEventListener('DOMContentLoaded', () => {
+    const signupBtn = document.getElementById('signup-btn');
+    const loginBtn = document.getElementById('login-btn');
+
+    if (signupBtn) signupBtn.onclick = () => auth('signup');
+    if (loginBtn) loginBtn.onclick = () => auth('login');
+});
+
+
 async function auth(type) {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
@@ -30,3 +39,19 @@ async function auth(type) {
 
 document.getElementById('signup-btn').onclick = () => auth('signup');
 document.getElementById('login-btn').onclick = () => auth('login');
+
+const authTitle = document.getElementById('auth-title');
+const loginBtn = document.getElementById('login-btn');
+const signupBtn = document.getElementById('signup-btn');
+const switchAuth = document.getElementById('switch-auth');
+
+let isLogin = true;
+
+switchAuth.onclick = () => {
+  isLogin = !isLogin;
+  authTitle.textContent = isLogin ? 'Se connecter' : "S'inscrire";
+  loginBtn.style.display = isLogin ? 'block' : 'none';
+  signupBtn.style.display = isLogin ? 'none' : 'block';
+  switchAuth.textContent = isLogin ? "Pas encore de compte ? S'inscrire" : "Déjà un compte ? Se connecter";
+};
+
